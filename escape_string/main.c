810b7333529218bc5957fdd9b43f690fca45be95
Arthur CHAZAL <achazal@e2r2p19.42.fr>
2015-11-23 19:06:52 +0100

    Eat, double quotes on the way

--- escape_string/main.c
+++ escape_string/main.c
@@ -5,96 +5,21 @@
 
 #include <stdio.h>
 
-// #define STRING "abc \t \t\t 'd e f'"
-#define STRING "abc     'd \te  \t f'"
+#define STRING "abc\\ \"d \\b f\" ls"
 
-void			handle_spaces(char **str, size_t *len)
+void			handle_backslash(char **str, size_t *len)
 {
-	char		*ptr;
-
-	**str = '\0';
-	ptr = ++*str;
-	while (*ptr == ' ' || *ptr == '\t')
-		++ptr;
-	memcpy(*str, ptr, *len - (ptr - *str));
-	*len -= ptr - *str + 1;
-}
-
-void			handle_simple_quotes(char **str, size_t *len)
-{
-	char		*ptr;
-
-	ptr = *str;
-	while (*ptr && *ptr != '\'')
-		++ptr;
-	if (!*ptr)
+	printf("Before handle_backslash:\t[%s]\n", *str);
+	if (!(*str)[1])
 	{
 		*str = NULL;
 		return ;
 	}
-
-	// printf("Before: [%s], len: %lu", *str, *len - (ptr - *str) - 3);
-	printf("Before: [%s]", *str);
-	memcpy(*str, *str + 1, *len - (ptr - *str + 1));
-	printf(" after: [%s], ptr: [%s]\n", *str, ptr);
-	// printf(" new len: %lu", *len - 3);
-					// (*str)[*len - 3] = '\0';
-	// printf(" After: [%s]\n", *str);
-
-	// *str = ptr - 2;
+	memcpy(*str, *str + 1, *len - 1);
+	(*str)[*len - 1] = '\0';
+	*str += 1;
 	*len -= 2;
-	printf("*str: [%s], *len: %zu\n", *str, *len);
-	// (*str)[*len] = '\0';
-}
-
-// void			handle_double_quotes(char **str, size_t *len)
-// {
-// 	char		*ptr;
-
-// 	ptr = ++*str;
-// 	while (*ptr && *ptr != '"')
-// 		++ptr;
-// 	*str = *ptr;
-// }
-
-t_escape		escape_string(char *str)
-{
-	t_escape	escaped;
-	size_t		len;
-
-	escaped.string = str;
-	if (!*escaped.string)
-	{
-		escaped.size = 0;
-		return (escaped);
-	}
-	escaped.size = 1;
-	len = strlen(str);
-	while (*str)
-	{
-		printf("\t\tString: [%s]\n", str);
-		if (*str == ' ' || *str == '\t')
-		{
-			handle_spaces(&str, &len);
-			++escaped.size;
-		}
-		else if (*str == '\'')
-			handle_simple_quotes(&str, &len);
-		// else if (*str == '\"')
-		// 	handle_double_quotes(&str, &len);
-		else
-		{
-			++str;
-			--len;
-		}
-		if (!str)
-		{
-			free(str);
-			escaped.string = NULL;
-			escaped.size = 0;
-		}
-	}
-	return (escaped);
+	printf("After  handle_backslash:\t[%s]\n", *str);
 }
 
 static void		do_put_escaped(char *begin, char *ptr, size_t size)
@@ -122,6 +47,122 @@ void			put_escaped(const t_escape escaped)
 		do_put_escaped(escaped.string, escaped.string, escaped.size);
 }
 
+void			handle_spaces(char **str, size_t *len)
+{
+	char		*ptr;
+
+	printf("Before handle_spaces:\t\t[%s]\n", *str);
+	**str = '\0';
+	ptr = ++*str;
+	while (*ptr == ' ' || *ptr == '\t')
+		++ptr;
+	memcpy(*str, ptr, *len - (ptr - *str));
+	*len -= ptr - *str + 1;
+	printf("After  handle_spaces:\t\t[%s]\n", *str);
+}
+
+void			handle_simple_quotes(char **str, size_t *len)
+{
+	char		*ptr;
+
+	printf("Before handle_simple_quotes:\t[%s]\n", *str);
+	ptr = *str + 1;
+	while (*ptr && *ptr != '\'')
+		++ptr;
+	if (!*ptr)
+	{
+		*str = NULL;
+		return ;
+	}
+	memcpy(*str, *str + 1, *len - (ptr - *str) + 1);
+	memcpy(ptr - 1, ptr + 1, *len - (ptr - *str));
+	*str = ptr - 1;
+	(*str)[*len - 2] = '\0';
+	*len -= 2;
+	printf("After  handle_simple_quotes:\t[%s]\n", *str);
+}
+
+void			handle_double_quotes(char **str, size_t *len)
+{
+
+	char		*ptr;
+
+	printf("Before handle_simple_quotes:\t[%s]\n", *str);
+	ptr = *str + 1;
+	while (*ptr && *ptr != '"')
+	{
+		if (*ptr == '\\')
+		{
+			handle_backslash(&ptr, len);
+			if (!ptr)
+			{
+				*str = NULL;
+				return ;
+			}
+		}
+		else
+			++ptr;
+	}
+	if (!*ptr)
+	{
+		*str = NULL;
+		return ;
+	}
+	printf("\tBefore memcpy 1: [%s]\n", *str);
+	memcpy(*str, *str + 1, *len - (ptr - *str) + 1);
+	printf("\tBefore memcpy 2: [%s]\n", *str);
+	memcpy(ptr - 1, ptr + 1, *len - (ptr - *str));
+	printf("\tAfter memcpies: [%s]\n", *str);
+	*str = ptr - 1;
+	(*str)[*len - 2] = '\0';
+	*len -= 2;
+	printf("After  handle_simple_quotes:\t[%s]\n", *str);
+}
+
+t_escape		escape_string(char *str)
+{
+	const char	*origin = str;
+	t_escape	escaped;
+	size_t		len;
+
+	escaped.string = str;
+	if (!*escaped.string)
+	{
+		escaped.size = 0;
+		return (escaped);
+	}
+	escaped.size = 1;
+	len = strlen(str);
+	while (*str)
+	{
+		if (*str == ' ' || *str == '\t')
+		{
+			handle_spaces(&str, &len);
+			++escaped.size;
+			printf("Space detected in %li\n", str - origin);
+			put_escaped(escaped);
+		}
+		else if (*str == '\'')
+			handle_simple_quotes(&str, &len);
+		else if (*str == '\"')
+			handle_double_quotes(&str, &len);
+		else if (*str == '\\')
+			handle_backslash(&str, &len);
+		else
+		{
+			++str;
+			--len;
+		}
+		if (!str)
+		{
+			free(str);
+			escaped.string = NULL;
+			escaped.size = 0;
+		}
+	}
+	return (escaped);
+}
+
 char			*for_each_escaped(t_escape *escaped)
 {
 	char		*ptr;
@@ -141,20 +182,7 @@ int				main(void)
 	char		*str;
 	t_escape	escaped;
 
-	// str = strdup(STRING);
-	// escaped = escape_string(str);
-	
-	// if (!(escaped.string = (char *)malloc(sizeof(char) * 4096)))
-	// 	return (-1);
-	// memcpy(escaped.string, TEST, sizeof(TEST));
-	// escaped.size = 2;
 	escaped = escape_string(strdup(STRING));
 	put_escaped(escaped);
-
-	// while ((str = for_each_escaped(&escaped)))
-		// printf("str = [%s]\n", str);
-		// str = for_each_escaped(&escaped);
-		// 	printf("str = [%s]\n", str);
-		// put_escaped(escaped);
 	return (0);
 }
